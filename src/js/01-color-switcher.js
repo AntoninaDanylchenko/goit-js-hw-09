950d932b3f46d9cf11c23b733d109dd604eb2eed
TosiaDan <antonina.ivanets@gmail.com>
2022-10-19 21:55:41 +0200

    push from desktop

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,33 @@
+refs = {
+  btnStartEl: document.querySelector('[data-start]'),
+  btnStopEl: document.querySelector('[data-stop]'),
+  bodyEl: document.querySelector('body'),
+};
+let timerId = null;
+
+refs.btnStopEl.disabled = true;
+
+refs.btnStartEl.addEventListener('click', onBtnStartClick);
+refs.btnStopEl.addEventListener('click', onBtnStopClick);
+
+function onBtnStartClick(e) {
+  e.preventDefault();
+  refs.btnStartEl.disabled = true;
+  refs.btnStopEl.disabled = false;
+  timerId = setInterval(() => {
+    // const colorBackgroundBody = getRandomHexColor();
+    refs.bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onBtnStopClick(e) {
+  e.preventDefault();
+  clearInterval(timerId);
+
+  refs.btnStartEl.disabled = false;
+  refs.btnStopEl.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
